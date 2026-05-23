4b2f4e1d3b550e00d6b077d2afd5ab1f09245a44
robypz <robertyepez0208@hotmail.com>
2026-05-23 18:11:51 +0200

    refactor: dynamic product category listing by integrating data-driven component architecture

--- src/app/products/product-service.ts
+++ src/app/products/product-service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal, Signal } from '@angular/core';
+import { Category } from '../categories/category.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+  private categories = signal<Category[]>([]);
+  private httpClient = inject(HttpClient);
 
+  constructor() {
+    this.loadCategories();
+  }
+
+  private loadCategories() {
+    this.httpClient.get<Category[]>('/data/data.json').subscribe({
+      next: (response) => {
+        this.categories.set(response);
+      }
+    });
+  }
+
+  public getCategories(): Signal<Category[]> {
+    return this.categories.asReadonly();
+  }
 }
--- src/app/products/products-home/products-home.ts
+++ src/app/products/products-home/products-home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../product-service';
 
 @Component({
   selector: 'app-products-home',
@@ -6,4 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './products-home.html',
   styleUrl: './products-home.scss',
 })
-export class ProductsHome {}
+export class ProductsHome {
+  private productService = inject(ProductService);
+  public categories = this.productService.getCategories();
+}
