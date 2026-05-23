d9cc38a4289949d68a81758ebedd188df41ec8c0
robypz <robertyepez0208@hotmail.com>
2026-05-23 19:50:44 +0200

    refactor: migrate product catalog to dynamic data rendering using signals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Catalog } from './products/catalog/catalog';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home
-    }
+    },
+    {
+        path: 'catalogo',
+        component: Catalog
+    },
 ];
--- src/app/products/catalog/catalog.ts
+++ src/app/products/catalog/catalog.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ProductService } from '../product-service';
+import { Product } from '../product.model';
+import { Category } from '../../categories/category.model';
 
 @Component({
   selector: 'app-catalog',
@@ -6,4 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './catalog.html',
   styleUrl: './catalog.scss',
 })
-export class Catalog {}
+export class Catalog implements OnInit {
+  private productService = inject(ProductService);
+  public products = signal<Product[]>([]);
+  public categories = signal<Category[]>([]);
+
+  ngOnInit(): void {
+    this.categories.set(this.productService.getCategories()());
+    this.categories().forEach((category) => {
+      this.products.update((value) => value.concat(category.products));
+    });
+
+  }
+
+
+}
