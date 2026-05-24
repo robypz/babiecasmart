e962df58ac62d864e2bf637844764bf19ed36f9b
robypz <robertyepez0208@hotmail.com>
2026-05-24 16:53:54 +0200

    feat: implement category-based product filtering logic in catalog view

--- src/app/products/catalog/catalog.ts
+++ src/app/products/catalog/catalog.ts
@@ -13,14 +13,42 @@ export class Catalog implements OnInit {
   private productService = inject(ProductService);
   public products = signal<Product[]>([]);
   public categories = signal<Category[]>([]);
+  private btn: any;
 
   ngOnInit(): void {
     this.categories.set(this.productService.getCategories()());
-    this.categories().forEach((category) => {
-      this.products.update((value) => value.concat(category.products));
-    });
+    this.loadAllProducts();
 
   }
 
+  loadAllProducts() {
+    this.categories().forEach((category) => {
+      this.products.update((value) => value.concat(category.products));
+    });
+  }
+
+  filter(name: string, event: any) {
+    var element = event.target as HTMLElement;
+    element.classList.toggle('active-filter');
+
+
+    if (element == this.btn) {
+      this.loadAllProducts();
+    }
+    else {
+      var filteredCategories = this.categories().filter((category) =>
+        category.name.toLowerCase().includes(name.toLowerCase())
+      );
+
+      this.products.set(
+        filteredCategories.reduce(
+          (acc, category) => acc.concat(category.products),
+          [] as Product[]
+        )
+      );
+    }
+
+    this.btn = element;
+  }
 
 }
