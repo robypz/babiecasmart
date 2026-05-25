a476bfde8d61dcfcba514a4d837aca19b3c415cc
robypz <robertyepez0208@hotmail.com>
2026-05-25 18:14:39 +0200

    contact email subject

--- src/app/contact/email/email.ts
+++ src/app/contact/email/email.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-email',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './email.html',
   styleUrl: './email.scss',
 })
-export class Email {}
+export class Email {
+  subject = '';
+  message = '';
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    const subjectEncoded = encodeURIComponent(this.subject);
+    const bodyEncoded = encodeURIComponent(this.message);
+    window.location.href = `mailto:info@babiecasmart.es?subject=${subjectEncoded}&body=${bodyEncoded}`;
+  }
+}
+
