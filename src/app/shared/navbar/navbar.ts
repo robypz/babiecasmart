587576beebe9ae638b6481f5e2276ad734f82e9d
robypz <robertyepez0208@hotmail.com>
2026-05-26 18:21:26 +0200

    feat: add responsive mobile menu toggle to navbar and update contact link destination

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -7,4 +7,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar { }
+export class Navbar {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
